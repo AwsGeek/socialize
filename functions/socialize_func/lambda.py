c1cca098ce251cd8366a758d3e27c6b84cd481de
Jerry Hargrove <jerry@awsgeek.com>
2019-11-07 10:19:55 -0800

    Simplified code

--- functions/socialize_func/lambda.py
+++ functions/socialize_func/lambda.py
@@ -1,22 +1,17 @@
 import pystache
-from urllib.parse import unquote, unquote_plus
+from urllib.parse import unquote_plus
 
 def handler(event, context):
   print(event)
   
   params = event['queryStringParameters']
   info = { 
-    'image' : unquote(params['image']) 
+    'image' : unquote_plus(params.get('image','')),
+    'title' : unquote_plus(params.get('title','')),
+    'description' : unquote_plus(params.get('description','')),
+    'url' : unquote_plus(params.get('url',''))
   }
-  if 'title' in params:
-    info['title'] = unquote_plus(unquote(params['title']))
-    
-  if 'description' in params:
-    info['description'] = unquote_plus(unquote(params['description']))
 
-  if 'url' in params:
-    info['url'] = unquote_plus(unquote(params['url']))
-  
   with open('note.mustache', 'r') as f:
     template = f.read()
     html =  pystache.render(template, info)
